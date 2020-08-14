527cf05b2c5d41fad7a1b9c033771299b16c68f4
Kostadin <kostadin.lalchev88@gmail.com>
2020-08-14 06:05:41 +0300

    Correct README Markdown

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,6 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
-const pkg = require('projectz');
 
 module.exports = {
     entry: './src/client/index.js',
